bbe4c9beafb9a8210e7609de0d5de40535b07a7f
Silas "Sai" Sadia <silaspsadia@gmail.com>
2017-06-01 22:28:39 -0700

    Added token-data converter and aggregator

--- midi_csv_texthandle.py
+++ midi_csv_texthandle.py
@@ -9,3 +9,23 @@ import itertools
 import string
 import sys
 import tokenize
+
+token_list = []
+prim_data_set = []
+trackl_data_set = []
+
+def split_token(token):
+	atoms = token.split(", ")
+	return atoms
+
+def clean_token(token_atoms):
+	cur = token_atoms[2][5] 
+	if cur == 'n':
+		token_atoms[2] = 1
+	elif cur == 'f':
+		token_atoms[2] = 0
+	else:
+		raise ValueError("Error: non-Note token element")
+
+for token in token_list:
+	prim_data_set += clean_token(split_token(token))\
